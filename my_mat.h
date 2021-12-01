8d97c474d46ba07f1475899b0f17bdb2fe675c3f
TairMazriv <80627174+TairMazriv@users.noreply.github.com>
2021-12-01 19:09:55 +0200

    Add files via upload

---
+++ my_mat.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define V 10
+#define INF 99999
+int MAT[V][V];
+int AM[V][V];
+void A();
+bool B( int i, int j);
+int C(int i , int j);
+
+void shortest_path(int MAT[V][V]); 
